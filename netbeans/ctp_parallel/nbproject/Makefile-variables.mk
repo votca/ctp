5e53ad082d9c39de7ce2b6471a2e8e93644e2a7b
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2018-03-16 12:48:26 +0100

    gaussian cleanup

--- netbeans/ctp_parallel/nbproject/Makefile-variables.mk
+++ netbeans/ctp_parallel/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
+CND_PLATFORM_Debug=GNU-Linux
 CND_ARTIFACT_DIR_Debug=../../src/tools
 CND_ARTIFACT_NAME_Debug=ctp_parallel
 CND_ARTIFACT_PATH_Debug=../../src/tools/ctp_parallel
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=ctpparallel.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ctpparallel.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/ctpparallel.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
 CND_ARTIFACT_NAME_Release=ctp_parallel
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/ctp_parallel
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/ctp_parallel
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
 CND_PACKAGE_NAME_Release=ctpparallel.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/ctpparallel.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/ctpparallel.tar
 #
 # include compiler specific variables
 #
