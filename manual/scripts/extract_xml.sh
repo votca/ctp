fa4ae28534e999731062696ad422c89c67cd8ae8
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2018-04-16 16:47:10 +0200

    inputs files fixes

---
+++ manual/scripts/extract_xml.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+xml_dir=${1}
+tex_dir=${2}
+
+echo ${tex_dir}
+
+xmls=`ls "$xml_dir"/*.xml`
+echo ${xmls}
+
+for xml in $xmls; do
+	echo ${xml} "--" ${tex_dir}/${xml}.tex
+	name=$(basename ${xml})
+	votca_property --file ${xml} --format TEX --level 2 >> ${tex_dir}/${name}.tex
+done
